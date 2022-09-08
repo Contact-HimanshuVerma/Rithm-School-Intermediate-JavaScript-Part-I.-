fe39007d0bc2c2d40e0f6970053dc1aec485f65e
Contact-HimanshuVerma <76530290+Contact-HimanshuVerma@users.noreply.github.com>
2022-09-08 19:55:52 +0530

    Create Dom.js

---
+++ Dom.js
@@ -0,0 +1,35 @@
+console.clear()
+
+
+
+// Select the section with an id of container without using querySelector.
+let secCont = document.getElementById("container");
+
+// Select the section with an id of container using querySelector.
+let secCount1 = document.querySelector("#container")
+// Select all of the list items with a class of "second".
+let classSec = document.querySelectorAll(".second")
+// Select a list item with a class of third, but only the list item inside of the ol tag.
+//let class1 = document.getElementsByTagName("ul").getAttribute("third")
+// Give the section with an id of container the let a = document.getElementsByTagName("div")
+secCont.innerHTML += "Hello";
+// Add the clinnerHTMLass main to the div with a class of footer.
+let b = document.getElementsByTagName("div")[1]
+b.classList.add("main")
+// Remove the class main on the div with a class of footer.
+b.classList.remove("main")
+// Create a new li element.
+let createEl = document.createElement("li")
+
+// Give the li the text "four".
+createEl.innerText = "four"
+// Append the li to the ul element.
+let x = document.getElementsByTagName("ul")
+//createEl.appendChild(x);
+
+// Loop over all of the lis inside the ol tag and give them a background color of "green".
+
+
+// Remove the div with a class of footer.
+
+
